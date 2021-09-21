a2e7d93e2baa43b4ff5b0ca51cb5b12c7770ea89
Mitch <mitchaarndt@gmail.com>
2021-09-21 10:09:15 -0400

    working nvs code

--- main/main.cpp
+++ main/main.cpp
@@ -31,8 +31,12 @@
 #include "Websocket.h"
 #include "driver/gpio.h"
 
+#include <esp_system.h>
+#include <nvs.h>
+
 static const char *TAG = "MAIN";
 
+// GPIO code
 #define GPIO_INPUT_IO_0 (gpio_num_t)26
 #define GPIO_INPUT_PIN_SEL (1ULL << GPIO_INPUT_IO_0)
 #define ESP_INTR_FLAG_DEFAULT 0
@@ -56,6 +60,8 @@ static void gpio_task_example(void *arg)
         }
     }
 }
+
+//wifi prov code
 
 /* Signal Wi-Fi events on this event-group */
 const int WIFI_CONNECTED_EVENT = BIT0;
@@ -198,6 +204,81 @@ static void wifi_prov_print_qr(const char *name, const char *pop, const char *tr
     esp_qrcode_generate(&cfg, payload);
 #endif /* CONFIG_APP_WIFI_PROV_SHOW_QR */
     ESP_LOGI(TAG, "If QR code is not visible, copy paste the below URL in a browser.\n%s?data=%s", QRCODE_BASE_URL, payload);
+}
+
+// nvs code
+
+#define REGISTER_TAG "isRegistered"
+
+int8_t nvs_read_i8(const char *key)
+{
+
+    int8_t val = 0; // value will default to 0, if not set yet in NVS
+
+    // Open
+    printf("\n");
+    printf("Opening Non-Volatile Storage (NVS) handle... ");
+    nvs_handle_t my_handle;
+    esp_err_t err = nvs_open("storage", NVS_READWRITE, &my_handle);
+    if (err != ESP_OK)
+    {
+        printf("Error (%s) opening NVS handle!\n", esp_err_to_name(err));
+    }
+    else
+    {
+        printf("Done\n");
+
+        // Read
+        printf("Reading isRegistered from NVS ... ");
+        err = nvs_get_i8(my_handle, key, &val);
+        switch (err)
+        {
+        case ESP_OK:
+            printf("Done\n");
+            printf("is %s = %d\n", key, val);
+            break;
+        case ESP_ERR_NVS_NOT_FOUND:
+            printf("The value is not initialized yet!\n");
+            break;
+        default:
+            printf("Error (%s) reading!\n", esp_err_to_name(err));
+        }
+
+        // Close
+        nvs_close(my_handle);
+    }
+    return val;
+}
+
+void nvs_write_i8(const char *key, int8_t val)
+{
+    // Open
+    printf("\n");
+    printf("Opening Non-Volatile Storage (NVS) handle... ");
+    nvs_handle_t my_handle;
+    esp_err_t err = nvs_open("storage", NVS_READWRITE, &my_handle);
+    if (err != ESP_OK)
+    {
+        printf("Error (%s) opening NVS handle!\n", esp_err_to_name(err));
+    }
+    else
+    {
+        // Write
+        printf("Updating %s to %d in NVS ... ", key, val);
+        err = nvs_set_i8(my_handle, key, val);
+        printf((err != ESP_OK) ? "Failed!\n" : "Done\n");
+
+        // Commit written value.
+        // After setting any values, nvs_commit() must be called to ensure changes are written
+        // to flash storage. Implementations may write to storage at other times,
+        // but this is not guaranteed.
+        printf("Committing updates in NVS ... ");
+        err = nvs_commit(my_handle);
+        printf((err != ESP_OK) ? "Failed!\n" : "Done\n");
+
+        // Close
+        nvs_close(my_handle);
+    }
 }
 
 extern "C" void app_main(void)
@@ -392,6 +473,10 @@ extern "C" void app_main(void)
     gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
     //hook isr handler for specific gpio pin
     gpio_isr_handler_add(GPIO_INPUT_IO_0, gpio_isr_handler, (void *)GPIO_INPUT_IO_0);
+
+    int8_t isRegistered = nvs_read_i8(REGISTER_TAG);
+    isRegistered++;
+    nvs_write_i8(REGISTER_TAG, isRegistered);
 
     // wifi_send(register_enqueue());
 
