2ec826c3f4d53ee1e05d4ec8c5167c1bd7d00ac0
Mitch Win Laptop <mitchaarndt@gmail.com>
2021-11-19 11:44:30 -0500

    building and flashing with no immediate errors with new pins

--- components/Config/Config.h
+++ components/Config/Config.h
@@ -1,15 +1,6 @@
 #pragma once
 
 /*************** BUTTON MANAGER ******************/
-// #define ROW_0_PIN GPIO_NUM_39
-// #define ROW_1_PIN GPIO_NUM_14
-// #define ROW_2_PIN GPIO_NUM_22
-// #define ROW_3_PIN GPIO_NUM_23
-
-// #define COL_0_PIN GPIO_NUM_12
-// #define COL_1_PIN GPIO_NUM_27
-// #define COL_2_PIN GPIO_NUM_33
-// #define COL_3_PIN GPIO_NUM_15
 #define ROW_0_PIN GPIO_NUM_33
 #define ROW_1_PIN GPIO_NUM_32
 #define ROW_2_PIN GPIO_NUM_35
@@ -21,43 +12,23 @@
 #define COL_3_PIN GPIO_NUM_26
 
 /*************** DISPLAY MANAGER ******************/
-// #define PIN_NUM_MISO GPIO_NUM_19
-// #define PIN_NUM_MOSI GPIO_NUM_18
-// #define PIN_NUM_SCLK GPIO_NUM_5
-// #define PIN_NUM_CS GPIO_NUM_16
-
-#define PIN_NUM_MISO GPIO_NUM_9
-#define PIN_NUM_MOSI GPIO_NUM_7
-#define PIN_NUM_SCLK GPIO_NUM_6
-// #define PIN_NUM_CS GPIO_NUM_11
-#define PIN_NUM_CS GPIO_NUM_22
+#define PIN_NUM_MISO GPIO_NUM_12
+#define PIN_NUM_MOSI GPIO_NUM_13
+#define PIN_NUM_SCLK GPIO_NUM_14
+#define PIN_NUM_CS GPIO_NUM_15
 
 /*************** LED MANAGER ******************/
-// #define LED_PIN GPIO_NUM_17
 #define LED_PIN GPIO_NUM_2
 
 /*************** MOTOR MANAGER ******************/
-#define MOTOR_DRIVER_PIN GPIO_NUM_8
+#define MOTOR_DRIVER_PIN GPIO_NUM_25
 
 /*************** SHIP MANAGER ******************/
-// #define MUX_ROW_SEL_0 GPIO_NUM_21
-// #define MUX_ROW_SEL_1 GPIO_NUM_26
-// #define MUX_ROW_SEL_2 GPIO_NUM_25
-// #define MUX_COL_SEL_0 GPIO_NUM_4
-// #define MUX_COL_SEL_1 GPIO_NUM_13
-// #define MUX_COL_SEL_2 GPIO_NUM_32
-// #define BOAT_INPUT GPIO_NUM_34
-
 #define MUX_ROW_SEL_0 GPIO_NUM_18
 #define MUX_ROW_SEL_1 GPIO_NUM_19
 #define MUX_ROW_SEL_2 GPIO_NUM_21
-#define MUX_COL_SEL_0 GPIO_NUM_17 // change
+#define MUX_COL_SEL_0 GPIO_NUM_17
 #define MUX_COL_SEL_1 GPIO_NUM_4
-#define MUX_COL_SEL_2 GPIO_NUM_16 // change
-#define BOAT_INPUT GPIO_NUM_5
+#define MUX_COL_SEL_2 GPIO_NUM_16
 
-// hardware
-// io12
-// io13
-// io14
-// io15
+#define BOAT_INPUT GPIO_NUM_5
