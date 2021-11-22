2b637e93b74d8280e7aede66f920efa44f0e8193
Mitch Win Laptop <mitchaarndt@gmail.com>
2021-11-21 17:03:23 -0700

    added developement pins option to config.h

--- components/Config/Config.h
+++ components/Config/Config.h
@@ -1,5 +1,41 @@
 #pragma once
 
+#define DEVELOPEMENT_PINS
+
+#ifdef DEVELOPEMENT_PINS
+/*************** BUTTON MANAGER ******************/
+#define ROW_0_PIN GPIO_NUM_39
+#define ROW_1_PIN GPIO_NUM_14
+#define ROW_2_PIN GPIO_NUM_22
+#define ROW_3_PIN GPIO_NUM_23
+
+#define COL_0_PIN GPIO_NUM_12
+#define COL_1_PIN GPIO_NUM_27
+#define COL_2_PIN GPIO_NUM_33
+#define COL_3_PIN GPIO_NUM_15
+
+/*************** DISPLAY MANAGER ******************/
+#define PIN_NUM_MISO GPIO_NUM_19
+#define PIN_NUM_MOSI GPIO_NUM_18
+#define PIN_NUM_SCLK GPIO_NUM_5
+#define PIN_NUM_CS GPIO_NUM_16
+
+/*************** LED MANAGER ******************/
+#define LED_PIN GPIO_NUM_17
+
+/*************** MOTOR MANAGER ******************/
+#define MOTOR_DRIVER_PIN GPIO_NUM_8
+
+/*************** SHIP MANAGER ******************/
+#define MUX_ROW_SEL_0 GPIO_NUM_21
+#define MUX_ROW_SEL_1 GPIO_NUM_26
+#define MUX_ROW_SEL_2 GPIO_NUM_25
+#define MUX_COL_SEL_0 GPIO_NUM_4
+#define MUX_COL_SEL_1 GPIO_NUM_13
+#define MUX_COL_SEL_2 GPIO_NUM_32
+#define BOAT_INPUT GPIO_NUM_34
+
+#else
 /*************** BUTTON MANAGER ******************/
 #define ROW_0_PIN GPIO_NUM_33
 #define ROW_1_PIN GPIO_NUM_32
@@ -32,3 +68,4 @@
 #define MUX_COL_SEL_2 GPIO_NUM_16
 
 #define BOAT_INPUT GPIO_NUM_5
+#endif
